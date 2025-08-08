b07d6c672640e1f816604e6fb395dac869ea0935
Anderson10567 <grisinandrej476@gmail.com>
2025-08-08 23:46:39 +0300

    feat: initial commit for test task

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
+  
+  base: '/Sakura-table/', 
+
   plugins: [react()],
    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
    // @ts-ignore
   test: {
-    environment: "jsdom", // браузерный контекст для React Testing Library
-    globals: true,        // чтобы писать тесты без импорта expect и др.
-    setupFiles: "./src/test/setup.ts", // путь к файлу с настройками
+    environment: "jsdom", 
+    globals: true,        
+    setupFiles: "./src/test/setup.ts", 
   },
 })
